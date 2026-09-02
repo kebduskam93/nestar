19d191de489b25ec30317791c4e8c30a72c6b798
maksud <maksud0217@icloud.com>
2026-09-02 15:26:06 +0900

    feat: develop updatePropertyByAdmin GraphQL api

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -231,4 +231,32 @@ private shapeMatchQuery(match: T, input: PropertiesInquiry): void {
   return result[0];
 }
 
+   public async updatePropertyByAdmin(input: PropertyUpdate): Promise<Property> {
+  let { propertyStatus, soldAt, deletedAt } = input;
+  const search: T = {
+    _id: input._id,
+
+  };
+
+  if (propertyStatus === PropertyStatus.SOLD) soldAt = moment().toDate();
+  else if (propertyStatus === PropertyStatus.DELETE) deletedAt = moment().toDate();
+
+  const result = await this.propertyModel
+    .findOneAndUpdate(search, input, {
+      new: true,
+    })
+    .exec();
+  if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+
+  if (soldAt || deletedAt) {
+    await this.memberService.memberStatsEditor({
+      _id: result.memberId,
+      targetKey: 'memberProperties',
+      modifier: -1,
+    });
+  }
+
+  return result;
+}
+
 }
