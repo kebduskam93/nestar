d2bad22c2f152f3a3dd327bc3a7f8ba4244f44bb
maksud <maksud0217@icloud.com>
2026-09-02 10:19:24 +0900

    feat: develop updateProperty  business logic

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -10,6 +10,7 @@ import { AuthMember } from '../auth/decorators/authMember.decorator';
 import {  type ObjectId } from 'mongoose';
 import { WithoutGuard } from '../auth/guards/without.guard';
 import { shapeIntoMongoObjectId } from '../../libs/config';
+import { PropertyUpdate } from '../../libs/dto/property/property.update';
 
 
 @Resolver()
@@ -38,5 +39,17 @@ public async getProperty(
   const propertyId = shapeIntoMongoObjectId(input);
   return await this.propertyService.getProperty(memberId, propertyId);
 }
+
+@Roles(MemberType.AGENT)
+  @UseGuards(RolesGuard)
+  @Mutation((returns) => Property)
+  public async updateProperty(
+    @Args('input') input: PropertyUpdate,
+    @AuthMember('_id') memberId: ObjectId,
+  ): Promise<Property> {
+    console.log('Mutation: updateProperty');
+    input._id = shapeIntoMongoObjectId(input._id);
+    return await this.propertyService.updateProperty(memberId, input);
+  }
    
 }
--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -9,6 +9,9 @@ import { PropertyStatus } from '../../libs/enums/property.enum';
 import { StatisticModifier, T } from '../../libs/types/common';
 import { ViewGroup } from '../../libs/enums/view.enum';
 import { ViewService } from '../view/view.service';
+import { PropertyUpdate } from '../../libs/dto/property/property.update';
+import moment from 'moment';
+
 
 @Injectable()
 export class PropertyService {
@@ -68,4 +71,34 @@ export class PropertyService {
     )
     .exec();
 }
+
+  public async updateProperty(memberId: ObjectId, input: PropertyUpdate): Promise<Property> {
+  let { propertyStatus, soldAt, deletedAt } = input;
+  const search: T = {
+    _id: input._id,
+    memberId: memberId,
+    propertyStatus: PropertyStatus.ACTIVE,
+  };
+
+  if (propertyStatus === PropertyStatus.SOLD) soldAt = moment().toDate();
+  else if (propertyStatus === PropertyStatus.DELETE) deletedAt = moment().toDate();
+
+  const result = await this.propertyModel
+    .findOneAndUpdate(search, input, {
+      new: true,
+    })
+    .exec();
+  if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+
+  if (soldAt || deletedAt) {
+    await this.memberService.memberStatsEditor({
+      _id: memberId,
+      targetKey: 'memberProperties',
+      modifier: -1,
+    });
+  }
+
+  return result;
+}
+
 }
